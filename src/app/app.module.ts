428305c0fa1ebf05579df041a1968fc43255e98d
Varshini <varshuk01@gmail.com>
2022-10-15 09:20:49 +0530

    binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewallTrainsComponent } from './viewall-trains/viewall-trains.component';
 
+
+const appRoutes:Routes=[
+  {
+    path:"",component: ViewallTrainsComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewallTrainsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/viewall-trains/viewall-trains.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewall-trains',
+  templateUrl: './viewall-trains.component.html',
+  styleUrls: ['./viewall-trains.component.css']
+})
+export class ViewallTrainsComponent implements OnInit {
+
+  constructor() { }
+name="john"
+  ngOnInit(): void {
+  }
+
+}
